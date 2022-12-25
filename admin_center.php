3299ab5d8c40a499967abd14e5170b5c08a71cc1
unknown <dullstart.yang.com>
2022-12-25 13:03:42 +0800

    icon link

--- admin_center.php
+++ admin_center.php
@@ -1,1 +1,35 @@
-admin.php
+<?php include_once "./db/base.php"; ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/front.css">
+    <title>Opinion</title>
+    <?php include_once "./layouts/link_css.php" ?>
+</head>
+
+<body>
+    <?php include_once "./layouts/header.php"; ?>
+    <?php
+    $do = $_GET['do'] ?? 'main';
+    $file = "./front/" . $do . ".php";
+    if (file_exists($file)) {
+        include_once $file;
+    } else {
+        include_once "./front/main.php";
+    }
+    $do = $_GET['do'] ?? 'main';
+    $file = "./front/" . $do . ".php";
+    if (file_exists($file)) {
+        include_once $file;
+    } else {
+        include_once "./front/main.php";
+    }
+    ?>
+    <a href="index.php?do=login">123</a>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -6,7 +6,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/front.css">
     <title>Opinion</title>
+    <?php include "./layouts/link_css.php" ?>
 </head>
 
 <body>
@@ -19,15 +21,12 @@
     } else {
         include_once "./front/main.php";
     }
-    $do = $_GET['do'] ?? 'main';
-    $file = "./front/" . $do . ".php";
-    if (file_exists($file)) {
-        include_once $file;
-    } else {
-        include_once "./front/main.php";
-    }
+  
     ?>
-    <a href="index.php?do=123">123</a>
+   
+    <i class="bi bi-incognito" style="font-size:30px; color:red;"></i>
+   
+</svg>
 </body>
 
 </html>
