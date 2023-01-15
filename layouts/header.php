c3749485a53d8714ff9e25a2290962c3528cdafe
unknown <dullstart.yang.com>
2023-01-15 20:35:23 +0800

    header nav

--- layouts/header.php
+++ layouts/header.php
@@ -2,13 +2,28 @@
     .i {
         font-size: 28px;
         margin-top: 20px;
-        vertical-align: middle;
-        color: var(--darkBlue);
+        vertical-align: baseline;
+        color: var(--blue);
+        position: relative;
+        top: -18px;
+        font-size: 32px;
+
+
     }
 
     .tc {
-        color: #eee;
-        font-size: 16px;
+        color: var(--blue);
+        font-size: 20px;
+        position: relative;
+        top: -18px;
+    }
+
+    .music {
+        color: var(--blue);
+        font-size: 40px;
+        position: relative;
+        top: 0px;
+        text-shadow: 1px 1px 3px var(--lightBlue);
     }
 </style>
 <header class="shadow" style="margin-bottom:56px;">
@@ -23,7 +38,7 @@
 
                 <ul class="nav nav-pills" role="tablist">
                     <li class="nav-item mx-3">
-                        <a class=" nav-link tc " href="index.php" type="button">logo</a>
+                        <a class=" nav-link tc " href="index.php"><i class="bi bi-file-earmark-music music"></i></a>
                     </li>
                 </ul>
 
@@ -31,35 +46,41 @@
 
                     <?php
 
-
-
-                    if (isset($_SESSION['login'])) {
-
+                    if (isset($_SESSION['admin']) || (isset($_SESSION['admin']) && isset($_SESSION['login']))) {
                     ?>
                         <li class="nav-item mx-2">
+                            <i class="bi bi-person-lines-fill i"></i>
+                        </li>
+                        <li class="nav-item mx-2"  style="padding-right:20px;">
+                            <a href="admin_center.php" class="nav-link tc" type="button">back manage</a>
+                        </li>
+
+
+                    <?php } else if (isset($_SESSION['login'])) {  ?>
+                        <li class="nav-item ">
                             <i class="bi bi-person-vcard-fill i" style="color:blue;"></i>
                         </li>
-                        <li class="nav-item mx-2">
+                        <li class="nav-item mx-2" style="padding-right:28px;">
                             <a href="?do=user_edit&id=<?= $_SESSION['login']['id'] ?>" class="nav-link tc" type="button">Member Center</a>
                         </li>
                         <li class="nav-item mx-2">
                             <i class="bi bi-door-open-fill i" style="color:blue;"></i>
                         </li>
-                        <li class="nav-item mx-2">
+                        <li class="nav-item mx-2"  style="padding-right:20px;">
                             <a href="./logout.php" class="nav-link tc" type="button">Sign Out</a>
                         </li>
 
                     <?php  } else { ?>
-                        <li class="nav-item mx-2">
+                        <li class="nav-item ">
                             <i class="bi bi-person-plus py-4 i"></i>
                         </li>
-                        <li class="nav-item mx-2">
+                        <li class="nav-item mx-2" style="padding-right:28px;">
                             <a href="?do=reg" class="nav-link tc" type="button">Sign Up</a>
                         </li>
                         <li class="nav-item mx-2">
                             <i class="bi bi-person-workspace i"></i>
                         </li>
-                        <li class="nav-item mx-2">
+                        <li class="nav-item mx-2" style="padding-right:20px;">
                             <a href="?do=login" class="nav-link tc" type="button">Login</a>
                         </li>
                     <?php }
@@ -73,29 +94,26 @@
 
                 <ul class="nav nav-pills" role="tablist">
                     <li class="nav-item mx-3">
-                        <a class="nav-link active" data-bs-toggle="tab" type="button">logo</a>
-                    </li>
-                    <li class="nav-item mx-2">
-                        <a class="nav-link active" data-bs-toggle="tab" type="button">網站名稱</a>
+                        <a class=" nav-link tc " href="index.php"><i class="bi bi-file-earmark-music music"></i></a>
                     </li>
                 </ul>
                 <ul class="nav nav-pills " role="tablist">
                     <li class="nav-item mx-2">
-                        <a href="?do=survey" class="nav-link" type="button">選項主題管理</a>
+                        <a href="?do=survey" class="nav-link tc" type="button">選項主題管理</a>
                     </li>
 
                     <li class="nav-item mx-2">
-                        <a href="?do=users&p=1" class="nav-link" type="button">會員管理</a>
+                        <a href="?do=users&p=1" class="nav-link tc" type="button">會員管理</a>
                     </li>
                 </ul>
                 <ul class="nav nav-pills mr-2 " role="tablist">
 
                     <li class="nav-item mx-2">
-                        <a  href="?do=admin" class="nav-link" type="button">管理者列表</a>
+                        <a href="?do=admin" class="nav-link tc" type="button">管理者列表</a>
                     </li>
 
-                    <li class="nav-item mx-2">
-                        <a href="./logout.php" class="nav-link" type="button">登出</a>
+                    <li class="nav-item mx-2" style="padding-right:20px;">
+                        <a href="./logout.php" class="nav-link tc" type="button">登出</a>
                     </li>
                 </ul>
 
