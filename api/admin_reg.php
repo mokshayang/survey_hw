715434bbd3adeb30d8098f7a0eb95f6ef9c1f032
moksha <dullstart.yang@gmail.com>
2023-01-17 11:15:21 +0800

    修改管理者 登入帳號編輯的問題

--- api/admin_reg.php
+++ api/admin_reg.php
@@ -1,12 +1,16 @@
 <?php include_once "../db/base.php";
 dd($_POST); 
+$original = $admin->find(['acc'=>$_SESSION['admin']]);
 if(isset($_POST['id'])){
     $row['id']=$_POST['id'];
     $row['acc']=$_POST['acc'];
     $row['pw']=$_POST['pw'];
     $admin->save($row);
-    ($_SESSION['admin']=="admin")??$_SESSION['admin']=$row['acc'];
-    
+    //當修改到自己的時候 !!
+    if($original['id'] == $_POST['id'] ){
+        ($_SESSION['admin'] = $_POST['acc']);   
+    }
+     
     to("../admin_center.php?do=admin");
 
 }else{
